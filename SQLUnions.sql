bdd1b93392a265295c3d17124db492c4148ec507
Joshua Williams <101890023+JoshxWill@users.noreply.github.com>
2023-07-24 13:29:55 -0500

    Update SQLUnions.sql

--- SQLUnions.sql
+++ SQLUnions.sql
@@ -1,9 +1,16 @@
-/*
-	Union, Union All
-*/
-
+--Union
 Select * 
 From [SQL Tutorial].dbo.EmployeeDemographics
-
+Union 
 Select *
 From [SQL Tutorial].dbo.WareHouseEmployeeDemographics
+Order By EmployeeID
+
+
+--Union All
+Select * 
+From [SQL Tutorial].dbo.EmployeeDemographics
+Union 
+Select *
+From [SQL Tutorial].dbo.WareHouseEmployeeDemographics
+Order By EmployeeID
